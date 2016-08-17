6ddb0485863fe12611c05b1448879439e69c3637
paige11 <ellis.paige.v@gmail.com>
2016-08-17 14:53:28 -0400

    Done

--- app.rb
+++ app.rb
@@ -2,5 +2,13 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do 
+    erb :newteam
+  end
 
+  post '/newteam' do
+    # binding.pry 
+    @team = params.values
+    erb :team
+  end
 end
